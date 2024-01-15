74a40057f207301d2bae28318663a5649237adde
Adrianela1 <nemesiososo@gmail.com>
2024-01-15 13:23:42 -0600

    funcion recorrer nombre

--- index.js
+++ index.js
@@ -10,10 +10,27 @@ function sumarVariables (n1, n2) {
     return n1+n2
 }
 
-console.log( prueba1)
+// console.log( prueba1)
 
 console.log("O avisen");
 const greet = (name) => console.log(`Hi ${name}`);
 greet("Luis");
 let names= ["Abigail", "Zabdiel","Fernando"];
 
+
+let nombreCompleto = [[ 'Juan', 'Carlos' ], 'Pérez'];
+function recorrerNombre(nombre) {
+    // Verificar si el nombre comienza con un array
+    if (Array.isArray(nombre[0])) {
+      // Recorrer el array al principio del nombre
+      for (let i = 0; i < nombre[0].length; i++) {
+        console.log(nombre[0][i]);
+      }
+    }
+    // Recorrer el resto del nombre (cadena de texto)
+    for (let i = nombre[0].length; i < nombre.length; i++) {
+      console.log(nombre[i]);
+    }
+  }
+  recorrerNombre(nombreCompleto);
+  

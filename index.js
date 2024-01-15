f5eb384278139a45a5df1e3b5f8918061bca7a0c
3liGonzalez <joaquina@MacBook-Pro-de-JOAQUINA.local>
2024-01-15 12:26:13 -0600

    subir cambios

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Hola Error 404
 console.log("no manden troyanos por favor");
+console. log ("no me gusta usar la terminal jajaja")
+/** Maca Bazar */
+function sumarVariables (n1, n2) {
+    return n1+n2
+}

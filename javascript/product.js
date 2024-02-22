8bf6fdedd90f0929ed7d6a1badd1232d48230bf7
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-02-22 03:25:08 -0600

    PROYEC-56 Add cart funcionality

--- javascript/product.js
+++ javascript/product.js
@@ -41,7 +41,3 @@ window.addEventListener("DOMContentLoaded", async () => {
         console.error("No se pudo obtener el producto.");
     }
 });
-
-document.querySelector(".btn-cart").addEventListener("click", function() {
-    alert("Producto agregado al carrito");
-});

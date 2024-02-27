b4748086b6081fe89c23b053180c638046372ade
eli <eli.hispanicas.unam@gmail.com>
2024-02-27 11:21:38 -0600

    PROYEC-62-Cart-total calculo compra actualizacion

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,6 +1,7 @@
 const listProducts = document.querySelector("#list-products");
 const cartProducts = [];
 const PRODUCTS_API = "https://fakestoreapi.com/products/";
+const getPurchaseTotal = document.getElementById("total");
 
 const fetchProduct = async (productId) => {
     try {
@@ -11,6 +12,20 @@ const fetchProduct = async (productId) => {
         return null;
     }
 };
+
+const getTotalCart = () => {
+    let total = 0;
+console.log(cartProducts);
+    // Iterar sobre cada producto en el array cartProducts
+    cartProducts.forEach((product) => {
+        // Asumiendo que cada producto tiene una propiedad "price"
+        // Sumar el precio del producto al total
+        total += product.price;
+    });
+
+    // El resultado estará en la variable "total"
+    getPurchaseTotal.textContent = total
+}
 
 const createCartItemCard = ({ id, title, price, image }) => {
     // Definir elementos
@@ -82,6 +97,7 @@ const createCartItemCard = ({ id, title, price, image }) => {
         const productId = buttonDelete.dataset.productId;
         deleteItemCart(productId);
         card.remove(); // También puedes eliminar directamente el elemento del DOM si es necesario
+        getTotalCart();
     });
 
     listProducts.appendChild(card);
@@ -118,7 +134,7 @@ const deleteItemCart = (productId) => {
 window.addEventListener("DOMContentLoaded", async () => {
     const productsLocalStorage = localStorage.getItem("products");
     const productsArray = JSON.parse(productsLocalStorage);
-    const getPurchaseTotal = document.getElementById("total");
+   
 
     for (const productId of productsArray) {
         const product = await fetchProduct(productId);
@@ -132,17 +148,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         createCartItemCard(product);
     });
     // Declarar una variable para almacenar la suma de los precios de los productos
-    let total = 0;
-
-    // Iterar sobre cada producto en el array cartProducts
-    cartProducts.forEach((product) => {
-        // Asumiendo que cada producto tiene una propiedad "price"
-        // Sumar el precio del producto al total
-        total += product.price;
-    });
-
-    // El resultado estará en la variable "total"
-    getPurchaseTotal.textContent= total
+    getTotalCart();
 });
 // Definir el carrito de compras como un array de objetos
 /*const cart = [
